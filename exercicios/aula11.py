b2a5c073ebfd8f5e6c9d482a0ad11f0068f0e27d
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-06-09 15:58:35 -0300

    Cores no terminal

---
+++ exercicios/aula11.py
@@ -0,0 +1,7 @@
+nome = 'Eduardo S.'
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[7;30m'}
+
+print('Olá, muito prazer em te conhecer, {}{}{}!!!'.format(cores['azul'], nome, cores['limpa']))
