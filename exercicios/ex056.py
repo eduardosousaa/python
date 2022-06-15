14f64f8eaf5764fdb150146f74c6b11b4ec2934f
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-06-15 09:39:06 -0300

    For no python

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,4 +1,23 @@
-for c in range(0, 4):
-    nome = str(input('Nome: '))
-    idade = int(input('Idade: '))
-    sexo = str(input('Sexo: '))
+cont = 0
+media = 0
+idadevelho = 0
+nomevelho = ''
+mulheres20 = 0
+for p in range(1, 5):
+    print('\033[m''----- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ''\033[32m')).strip()
+    idade = int(input('\033[m''Idade: ''\033[32m'))
+    sexo = str(input('\033[m''Sexo [M/F]: ''\033[32m')).strip().upper()
+    cont += idade
+    media = cont / 4
+    if p == 1 and sexo in 'M':
+        idadevelho = idade
+        nomevelho = nome
+    if idade > idadevelho:
+        idadevelho = idade
+        nomevelho = nome
+    if sexo in 'F' and idade < 20:
+        mulheres20 += 1
+print('\033[m''A média de idade do grupo é de {:.1f} anos.'.format(media))
+print('O homem mais velhor tem {} anos e se chama {}.'.format(idadevelho, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(mulheres20))
