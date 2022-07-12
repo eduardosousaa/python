a4faa3cbb1b5561c621e94608f34c53d1019708b
Eduardo de Sousa <97894971+eduardosousaa@users.noreply.github.com>
2022-07-12 09:44:26 -0300

    fim exercícios 1

---
+++ exercicios/ex071.py
@@ -0,0 +1,26 @@
+print('=' * 20)
+print('       Banco')
+print('=' * 20)
+saque = int(input('Quantas notas você quer sacar: '))
+total = saque
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('=' * 20)
+print('Volte Sempre!')
+
